6437abce37cfb6c4b4a53336e75e44de372f531b
Muhammet İNCE <83353864+MuhammetInceee@users.noreply.github.com>
2023-07-12 03:50:50 +0300

    Enemy Shader Completed

--- Assets/Dev/Scripts/Enemy/EnemyManager.cs
+++ Assets/Dev/Scripts/Enemy/EnemyManager.cs
@@ -2,21 +2,36 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    private const string MaterialSlierName = "_SplitPosition";
+    
     [SerializeField] private float health;
+    [SerializeField] private SkinnedMeshRenderer character;
     
     private Animator _animator;
+    private Material _material;
     private bool _canHit = true;
     
-    private void Awake() => _animator = GetComponent<Animator>();
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _material = character.material;
+    }
     
     private void OnTriggerEnter(Collider other)
     {
         if (!other.TryGetComponent(out Bullet bullet) || !_canHit) return;
         
-        health -= 10;
+        GetHit(10);
         bullet.GetBackPool();
         
         if (health <= 0) Murdered();
+    }
+    
+    private void GetHit(float splitPosition)
+    {
+        
+        health -= splitPosition;
+        _material.SetFloat(MaterialSlierName, health / 100);
     }
 
     private void Murdered()
